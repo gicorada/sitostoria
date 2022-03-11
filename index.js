9df03f2fbf9b873266cd8bd43da5c079b27984ab
Giacomo R <gradaellig@protonmail.com>
2022-03-11 19:02:57 +0100

    aggiunto link a file brochure

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //FOOTER 		|||
 //				VVV
 
-var footer = '<div class="content has-text-centered"><img src="media/Buddha-footer.svg" alt="" class="vectorial-image is-centered" height="150" width="150"><br><br><p>Sito creato da <a href="https://github.com/GicoProgram" target="_blank">Giacomo R.</a>, Nicolò B., Federico T., Fabio F.</p></div>';
+var footer = '<div class="content has-text-centered"><img src="media/Buddha-footer.svg" alt="" class="vectorial-image is-centered" height="150" width="150"><br><br><p>Sito creato da <a href="https://github.com/GicoProgram" target="_blank">Giacomo R.</a>, Nicolò B., Federico T., Fabio F.</p><a class="is-size-7" href="media/brochure.pdf">PDF</a></div>';
 
 document.getElementById("footer").innerHTML = footer;
