6d4991f5302186951e3d0066625b371bfa3bdeed
Giacomo Radaelli <gradaellig@protonmail.com>
2022-07-26 15:07:06 +0200

    First commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# sitostoria
-
-Questo è il repository del sito creato per il lavoro di storia sul Buddhismo
--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-//FOOTER 		|||
-//				VVV
+var header = '<div class="content has-text-centered"><img src="media/Buddha-footer.svg" alt="" class="vectorial-image is-centered" height="150" width="150"><br><br><p>Sito creato da <a href="https://github.com/GicoProgram" target="_blank">Giacomo R.</a>, Nicolò B., Federico T., Fabio F.</p><a class="is-size-7" href="media/brochure.pdf">PDF</a></div>';
 
-var footer = '<div class="content has-text-centered"><img src="media/Buddha-footer.svg" alt="" class="vectorial-image is-centered" height="150" width="150"><br><br><p>Sito creato da <a href="https://github.com/GicoProgram" target="_blank">Giacomo R.</a>, Nicolò B., Federico T., Fabio F.</p><a class="is-size-7" href="media/brochure.pdf">PDF</a></div>';
-
-document.getElementById("footer").innerHTML = footer;
+document.getElementById("header").innerHTML = header;
