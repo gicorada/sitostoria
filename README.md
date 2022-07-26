2da695e9d05a7ec8a38ec1e22eccfbd73600ce58
Giacomo Radaelli <gradaellig@protonmail.com>
2022-07-26 15:12:02 +0200

    Revert "First commit"
    This reverts commit 6d4991f5302186951e3d0066625b371bfa3bdeed.

---
+++ README.md
@@ -0,0 +1,3 @@
+# sitostoria
+
+Questo è il repository del sito creato per il lavoro di storia sul Buddhismo
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var header = '<div class="content has-text-centered"><img src="media/Buddha-footer.svg" alt="" class="vectorial-image is-centered" height="150" width="150"><br><br><p>Sito creato da <a href="https://github.com/GicoProgram" target="_blank">Giacomo R.</a>, Nicolò B., Federico T., Fabio F.</p><a class="is-size-7" href="media/brochure.pdf">PDF</a></div>';
+//FOOTER 		|||
+//				VVV
 
-document.getElementById("header").innerHTML = header;
+var footer = '<div class="content has-text-centered"><img src="media/Buddha-footer.svg" alt="" class="vectorial-image is-centered" height="150" width="150"><br><br><p>Sito creato da <a href="https://github.com/GicoProgram" target="_blank">Giacomo R.</a>, Nicolò B., Federico T., Fabio F.</p><a class="is-size-7" href="media/brochure.pdf">PDF</a></div>';
+
+document.getElementById("footer").innerHTML = footer;
